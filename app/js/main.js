7c8b1f7792727d017001f0030a80589828b47a59
elpeyotl <ch.perez@mac.com>
2018-10-18 23:37:29 +0200

    first

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,176 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
+/*********
+ * Opens closes dropdown
+ */
+Vue.component("dropdown-content", {
+  props: {
+    title: String,
+    lastItem: Boolean
+  },
+  data: function() {
+    return {
+      showContent: false,
+      isLastItem: this.lastItem
+    };
+  },
+  props: ["title", "lastItem"],
+  template: `
+  <div ref="dropdown">
+    <li class="question-box__list-item" :class="{'question-box__list-item--last' : isLastItem && !showContent, 'question-box__list-item--open': showContent }" @click="showContent = !showContent">{{title}}</li>
+    <transition name="fade-in">
+    <div class="question-box__content" :class="{'question-box__content--last' : isLastItem}" v-show="showContent">
+       <slot></slot>
+    </div>
+    </transition>
+  </div>`,
+  methods: {
+    documentClick: function(e) {
+      let el = this.$refs.dropdown;
+      let target = e.target;
+      if (el !== target && !el.contains(target)) {
+        this.showContent = false;
+      }
+    }
+  },
+  created() {
+    document.addEventListener("click", this.documentClick);
+  },
+  destroyed() {
+    // important to clean up!!
+    document.removeEventListener("click", this.documentClick);
   }
-})
+});
+
+/*********
+ * Opens closes dropdown
+ */
+Vue.component("lang-switcher", {
+  props: {
+    title: String
+  },
+  data: function() {
+    return {
+      dropdownIsOpen: false,
+      selected: "DE",
+      selection: ["DE", "FR", "EN"]
+    };
+  },
+  methods: {
+    onSelect: function(selection) {
+      this.selected = selection;
+      this.dropdownIsOpen = false;
+    }
+  },
+  template: `
+  <div class="lang-switcher-component">
+    <div class="lang-switcher__selected" @click="dropdownIsOpen = !dropdownIsOpen">{{selected}}
+        <img class="lang-switcher__arrow" src="assets/images/icon-arrow-down.svg" />
+    </div>
+    <transition name="fade">
+        <div v-show="dropdownIsOpen" class="lang-switcher__dropdown">
+            <ul class="lang-switcher__list">
+                <li @click="onSelect(sel)" v-for="sel in selection" :key="sel" class="lang-switcher__list-item" :class="{'lang-switcher__list-item--selected': sel === selected}">{{sel}}</li>
+            </ul>
+        </div>
+    </transition>
+ </div>`
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    showMenu: false,
+    mobileMenuOpen: false,
+    subPage: true,
+    subList: false,
+    scrolled: false,
+    subNavElements: {
+      versicherungen: true,
+      mehrwert: false,
+      ueberuns: false
+    },
+    subChildOpen: false,
+    dropdownIsOpen: false
+  },
+  computed: {
+    menuIsOpen: function() {
+      return this.showMenu ? true : false;
+    }
+  },
+  methods: {
+    showSubNav: function(el) {
+      this.showMenu = true;
+      const navItems = document.querySelectorAll(".main-header__nav-item");
+      for (let index = 0; index < navItems.length; index++) {
+        const element = navItems[index];
+        if (element.classList.contains("main-header__nav--hover-active"))
+          element.classList.remove("main-header__nav--hover-active");
+      }
+
+      //add active class to element
+      el.originalTarget.classList.add("main-header__nav--hover-active");
+
+      const navId = el.originalTarget.dataset.nav;
+      const subNavs = document.querySelectorAll(".main-header__sub-nav");
+      var subNav = null;
+
+      for (let index = 0; index < subNavs.length; index++) {
+        const element = subNavs[index];
+        //hide all subnavs
+        if (element.classList.contains("show")) {
+          element.classList.add("hide");
+          element.classList.remove("show");
+        }
+        if (element.dataset.navSub === navId) subNav = element;
+      }
+      if (subNav != null) {
+        subNav.classList.remove("hide");
+        subNav.classList.add("show");
+      }
+    },
+    hideNav: function(el) {
+      this.showMenu = false;
+      //add active class to element
+      el.originalTarget.classList.remove("main-header__nav--hover-active");
+
+      const subNavs = document.querySelectorAll(".main-header__sub-nav");
+      for (let index = 0; index < subNavs.length; index++) {
+        const element = subNavs[index];
+        //hide all subnavs
+        if (element.classList.contains("show")) {
+          element.classList.add("hide");
+          element.classList.remove("show");
+        }
+      }
+    },
+    showMobileSubNav: function(element) {
+      this.subNavElements[element] = true;
+      this.subPage = true;
+    },
+    mobileSubNavFirstPage: function() {
+      //reset subnavelements
+      for (var prop in this.subNavElements) {
+        this.subNavElements[prop] = false;
+      }
+      this.subPage = false;
+    },
+    toggleSublist: function(element) {
+      if (element.srcElement.classList.contains("list-item__plus")) {
+        console.log(1);
+        element.srcElement.classList.remove("list-item__plus");
+        element.srcElement.classList.add("list-item__close");
+        element.srcElement.nextElementSibling.classList.remove("display-none");
+      } else {
+        element.srcElement.classList.add("list-item__plus");
+        element.srcElement.classList.remove("list-item__close");
+        element.srcElement.nextElementSibling.classList.add("display-none");
+      }
+    }
+  },
+  mounted: function() {
+    const self = this;
+    document.addEventListener("scroll", function(event) {
+      console.log(event.pageY);
+      event.pageY > 116 ? (self.scrolled = true) : (self.scrolled = false);
+    });
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,102 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const useref = require('gulp-useref');
-const uglify = require('gulp-uglify');
-const gulpIf = require('gulp-if');
-const cssnano = require('gulp-cssnano');
-const imagemin = require('gulp-imagemin');
-const cache = require('gulp-cache');
-const autoprefixer = require('gulp-autoprefixer');
-const del = require('del');
-const runSequence = require('run-sequence');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const useref = require("gulp-useref");
+const uglify = require("gulp-uglify");
+const gulpIf = require("gulp-if");
+const cssnano = require("gulp-cssnano");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+const autoprefixer = require("gulp-autoprefixer");
+const del = require("del");
+const runSequence = require("run-sequence");
 
 //run SASS compiler
-gulp.task('sass', function () {
-    return gulp.src('app/stylesheets/sass/styles.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 version'],
-            grid: true
-            }
-         ))
-        .pipe(gulp.dest('app/stylesheets/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-})
+gulp.task("sass", function() {
+  return gulp
+    .src("app/stylesheets/sass/styles.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 version"]
+      })
+    )
+    .pipe(gulp.dest("app/stylesheets/"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
 
 //watch for changes
-gulp.task('watch', ['browserSync', 'sass'], function () {
-    //watch for SASS files changes and compile
-    gulp.watch('app/stylesheets/sass/**/*.scss', ['sass']);
-    //watch for HTML changes and reload browser
-    gulp.watch('app/*.html', browserSync.reload);
-    //watch for JS changes and reload browser
-    gulp.watch('app/js/*.js',browserSync.reload);
-})
+gulp.task("watch", ["browserSync", "sass"], function() {
+  //watch for SASS files changes and compile
+  gulp.watch("app/stylesheets/sass/**/*.scss", ["sass"]);
+  //watch for HTML changes and reload browser
+  gulp.watch("app/*.html", browserSync.reload);
+  //watch for JS changes and reload browser
+  gulp.watch("app/js/*.js", browserSync.reload);
+});
 
 //run Browsersync for automatic reload on changes
-gulp.task('browserSync', function () {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        }
-    })
-})
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+});
 
 //Minimizes JS and CSS Files
-gulp.task('useref', function () {
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        // Minifies only if it's a JavaScript file
-        .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulp.dest('dist'))
-        // Minifies only if it's a CSS file
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('dist'))
-})
+gulp.task("useref", function() {
+  return (
+    gulp
+      .src("app/*.html")
+      .pipe(useref())
+      // Minifies only if it's a JavaScript file
+      // .pipe(gulpIf("*.js", uglify()))
+      .pipe(gulp.dest("dist"))
+    // Minifies only if it's a CSS file
+    //   .pipe(gulpIf("*.css", cssnano()))
+    //   .pipe(gulp.dest("dist"))
+  );
+});
 
 //Minimizes Images
-gulp.task('images', function(){
-  return gulp.src('app/assets/images/**/*.+(png|jpg|gif|svg)')
-  .pipe(cache(imagemin()))
-  .pipe(gulp.dest('dist/assets/images'))
+gulp.task("images", function() {
+  return gulp
+    .src("app/assets/images/**/*.+(png|jpg|gif|svg)")
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest("dist/assets/images"));
 });
 
 //copy fonts to dist
-gulp.task('fonts', function () {
-    return gulp.src('app/assets/fonts/**/*')
-        .pipe(gulp.dest('dist/assets/fonts'))
-})
+gulp.task("fonts", function() {
+  return gulp.src("app/assets/fonts/**/*").pipe(gulp.dest("dist/assets/fonts"));
+});
+
+//copy fonts to dist
+gulp.task("css", function() {
+  return gulp.src("app/stylesheets/*.css").pipe(gulp.dest("dist/css/"));
+});
 
 //clean dist folder
-gulp.task('clean:dist', function () {
-    return del.sync('dist');
-})
+gulp.task("clean:dist", function() {
+  return del.sync("dist");
+});
 
 //clear cache (used in miniziming images)
-gulp.task('cache:clear', function (callback) {
-    return cache.clearAll(callback)
-})
+gulp.task("cache:clear", function(callback) {
+  return cache.clearAll(callback);
+});
 
 // Build for production minimizes js and css and images
-gulp.task('build', function (callback) {
-    runSequence('clean:dist',
-        ['useref', 'images', 'fonts'],
-        callback
-    )
-})
+gulp.task("build", function(callback) {
+  runSequence("clean:dist", ["useref", "css", "images", "fonts"], callback);
+});
 
 //default task for development
-gulp.task('default', function (callback) {
-    runSequence(['sass','browserSync', 'watch'],
-        callback
-    )
-})
-
+gulp.task("default", function(callback) {
+  runSequence(["sass", "browserSync", "watch"], callback);
+});
